f6580d26c207cc5be8df3e4dd4c0b72064f69bca
iitsuraj <34763906+iitsuraj@users.noreply.github.com>
2019-06-11 03:21:40 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database : 'mongodb://localhost:27017/dau',
+        // database: "mongodb://root:root123@ecommerce-shard-00-00-tf14p.mongodb.net:27017,ecommerce-shard-00-01-tf14p.mongodb.net:27017,ecommerce-shard-00-02-tf14p.mongodb.net:27017/dau?ssl=true&replicaSet=ecommerce-shard-0&authSource=admin&retryWrites=true",,
     port : process.env.PORT || 3000,
     secretKey: 'helloformserver'
 
